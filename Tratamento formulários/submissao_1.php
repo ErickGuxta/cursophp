daba41f484c7ae5dc4644fb459a24f6160cf4243
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2024-10-19 19:20:51 -0300

    .

--- Tratamento formulários/submissao_1.php
+++ Tratamento formulários/submissao_1.php
@@ -7,4 +7,46 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     die('Acesso inválido');
 }
 
+
+//vai buscar o nome do usuario e a senha
+
+
+$usuario=  isset($_POST['nome_usuario']) ? $_POST['nome_usuario'] : '';
+$senha=  isset($_POST['senha_usuario']) ? $_POST['senha_usuario'] : '';
+
+    // posso fazer dessa maneira (é a mesma coisa):
+
+        // if (isset($_POST['nome_usuario'])) {
+        //     $usuario = $_POST['nome_usuario'];
+        // } else{
+        //     $usuario = '';
+        // }
+
+        // if (isset($_POST['senha_usuario'])) {
+        //     $senha = $_POST['senha_usuario'];
+        // } else{
+        //     $senha = '';
+        // }
+
+//Verificar se o Login é valido
+    //de forma bruta:
+    $user = [
+        "usuario" => "erick",
+        "senha"  => "123"
+    ];
+
+
+    if ($usuario == $user['usuario'] && $senha ==  $user['senha']) {
+        echo '<h1>LOGIN VÁLIDO</h1>';
+    } else{
+        echo '<h1>LOGIN INVÁLIDO</h1>';
+
+    }
+
+//link para voltar
+
+echo '<a href="index.php">PÁGINA INICIAL</a>'
+
+
+//OBSERVAÇÃO:  EXISTEM ASPECTOS NESSE SCRIPT QUE NÃO SÃO BOAS PRÁTICAS
 ?>
