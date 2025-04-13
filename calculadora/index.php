d4a5961bd8a1a5ef2ecc33dc29ac7dcf6db0fe73
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-04-13 15:50:37 -0300

    .

---
+++ calculadora/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="display">0</div>
+        <div class="buttons">
+            <button class="button light-gray">C</button>
+            <button class="button light-gray">%</button>
+            <button class="button light-gray">.</button>
+            <button class="button orange">/</button>
+            <button class="button dark-gray">7</button>
+            <button class="button dark-gray">8</button>
+            <button class="button dark-gray">9</button>
+            <button class="button orange">*</button>
+            <button class="button dark-gray">4</button>
+            <button class="button dark-gray">5</button>
+            <button class="button dark-gray">6</button>
+            <button class="button orange">-</button>
+            <button class="button dark-gray">1</button>
+            <button class="button dark-gray">2</button>
+            <button class="button dark-gray">3</button>
+            <button class="button orange">+</button>
+            <button class="button dark-gray wide">0</button>
+            <button class="button orange wide">=</button>
+        </div>
+    </div>
+</body>
+<script src="script.js"></script>
+</html>
